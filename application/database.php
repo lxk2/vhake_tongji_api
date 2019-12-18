3d36c196c7b0c6a13756f4f532efaede507a49a0
lxk2 <737697366@qq.com>
2019-12-18 11:05:30 +0800

    abc

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'vhake_message_board',
+    'database'        => 'vhake_tongji',
     // 用户名
     'username'        => 'root',
     // 密码
